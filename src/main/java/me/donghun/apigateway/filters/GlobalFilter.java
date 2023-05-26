dd04e32f77a3cd98382ed7fd15ff599d8a264275
donghun93 <alswn4516@gmail.com>
2023-05-26 19:03:02 +0900

    Feat: Global Filter 수정

--- src/main/java/me/donghun/apigateway/filters/GlobalFilter.java
+++ src/main/java/me/donghun/apigateway/filters/GlobalFilter.java
@@ -1,9 +1,11 @@
 package me.donghun.apigateway.filters;
 
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
@@ -17,18 +19,32 @@ public class GlobalFilter extends AbstractGatewayFilterFactory<GlobalFilter.Conf
 
     @Override
     public GatewayFilter apply(Config config) {
-        return ((exchange, chain) -> {
+        // Global Pre Filter
+        return (exchange, chain) -> {
             ServerHttpRequest request = exchange.getRequest();
-            long begin = System.currentTimeMillis();
+            ServerHttpResponse response = exchange.getResponse();
 
-            //custom post filter
-            return chain.filter(exchange)
-                        .then(Mono.fromRunnable(() -> {
-                            log.info("GlobalFilter [{}] => {}ms", request.getPath(), System.currentTimeMillis() - begin);
-                        }));
-        });
+            long begin = System.currentTimeMillis();
+            log.info("Global Filter baseMessage : {}", config.getBaseMessage());
+
+            if(config.isPreLogger()){
+                log.info("Global Filter Start : request id -> {}", request.getId());
+            }
+
+            // Global Post Filter
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if(config.isPostLogger()) {
+                    log.info("Global Filter End : response code -> {}", response.getStatusCode());
+                    log.info("GlobalFilter [{}] => {}ms", request.getPath(), System.currentTimeMillis() - begin);
+                }
+            }));
+        };
     }
 
+    @Data
     public static class Config {
+        private String baseMessage;
+        private boolean preLogger;
+        private boolean postLogger;
     }
 }
